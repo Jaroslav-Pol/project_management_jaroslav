f33d93d333edc78a5c619ebcd84889722a4bf4fe
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-04-21 10:47:12 +0300

    Prideti notebook viewai i project_view.xml

--- models/project.py
+++ models/project.py
@@ -10,6 +10,10 @@ class Project(models.Model):
     description = fields.Text(string='Description')
     start_date = fields.Date(string='Start date')
     end_date = fields.Date(string='End date')
-    task_ids = fields.One2many('project_mngm_jp.task', 'project_id', string='Tasks')
-    invoice_ids = fields.One2many('project_mngm_jp.invoice', 'project_id', string='Invoice')
 
+    task_ids = fields.One2many('project_mngm_jp.task', 'project_id', string='Tasks')
+    invoice_ids = fields.One2many('project_mngm_jp.invoice', 'project_id', string='Invoices')
+
+    client_id = fields.Many2one('res.partner', string='Customer')
+    leader_id = fields.Many2one('hr.employee', string='Team leaderf')
+    employee_ids = fields.Many2many('hr.employee', string='Employees')
--- models/task.py
+++ models/task.py
@@ -6,5 +6,5 @@ class Task(models.Model):
     _name = 'project_mngm_jp.task'#pirmas yra modulio(aplanko) pav, kitas modelio
     _description = 'Tasks'
 
-    name = fields.Char(string='Task title', required=True)
+    name = fields.Char(string='Tasks', required=True)
     project_id = fields.Many2one('project_mngm_jp.project', string='Project')
